c2ee422a32b1b6517837e8875d3fc9947e175206
Spike Grobstein <me@spike.cx>
2023-08-12 17:05:31 -0700

    options: just use vim.opts directly

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
-opt.relativenumber = false -- Relative line numbers OFF
+vim.opt.relativenumber = false -- Relative line numbers OFF
 
 -- rust debugging support
 vim.api.nvim_command("packadd termdebug")
