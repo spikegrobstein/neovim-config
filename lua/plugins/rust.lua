53c8cd29c3691066ebca014f01f0db35cf7aae2f
Spike Grobstein <me@spike.cx>
2023-12-01 15:05:57 -0800

    removed some unused rust config
    this stuff lives in the rust config file

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,21 +1,5 @@
 return {
   'simrat39/rust-tools.nvim',
-  lazy = true,
-  opts = {
-    tools = {
-      --autoSetHints = true,
-      --hover_with_actions = true,
-      inlay_hints = {
-        auto = true,
-        only_current_line = false,
-        show_parameter_hints = false,
-        parameter_hints_prefix = '<- ',
-        other_hints_prefix = '=> ',
-        highlight = 'LspInlayHint',
-      },
-    },
-  },
-
   dependencies = {
     'neovim/nvim-lspconfig',
     'nvim-lua/plenary.nvim',
