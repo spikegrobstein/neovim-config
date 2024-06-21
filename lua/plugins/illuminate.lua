fd23e30863c84766f5c05ab4a29da199282334c3
Spike Grobstein <me@spike.cx>
2024-06-21 08:17:17 -0700

    illuminate now works
    just had to set up the proper events.

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -12,12 +12,13 @@ return {
     { "]]", desc = "Next Reference" },
     { "[[", desc = "Prev Reference" },
   },
+  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 
   -- I think this is just for LazyVim and not a normal lazy.nvim feature
   config = function(_, opts)
     require("illuminate").configure(opts)
 
-       local function map(key, dir, buffer)
+    local function map(key, dir, buffer)
       vim.keymap.set("n", key, function()
         require("illuminate")["goto_" .. dir .. "_reference"](false)
       end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
