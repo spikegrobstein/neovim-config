15d4276b27de5f0735d89a59bed9905c7b0d96e8
Spike Grobstein <me@spike.cx>
2024-07-22 09:11:05 -0700

    remove which-key
    I'm not really using this and they changed some of the structure of the
    config, which I don't feel like updating. but I'm keeping the stuff
    commented out just in case I come back to it in the future.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -44,14 +44,14 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
+--require("which-key").register {
+    --{ "<leader>c", group = "[C]ode" },
+    --{ "<leader>d", group = "[D]ocument" },
+    --{ "<leader>g", group = "[G]it" },
+    --{ "<leader>h", group = "More git" },
+    --{ "<leader>r", group = "[R]ename" },
+    --{ "<leader>w", group = "[W]orkspace" },
+  --}
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
