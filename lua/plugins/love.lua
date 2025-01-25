02723d6983b09fab614c8c732f9092bea9a354bc
Spike Grobstein <me@spike.cx>
2025-01-25 13:09:29 -0800

    when starting a love project, stop it first if it's currently running
    this includes a little half second wait before calling the function so
    that it has time to fully shut down. otherwise it's kinda hit or miss
    about whether it'll be able to start up again.

--- lua/plugins/love.lua
+++ lua/plugins/love.lua
@@ -3,10 +3,24 @@ return {
   "S1M0N38/love2d.nvim",
   cmd = "LoveRun",
   ft = "lua",
-  opts = { },
+  opts = {},
   keys = {
     --{ "<leader>v", ft = "lua", desc = "LÖVE" },
-    { "<leader>r", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
+    {
+      "<leader>r",
+      function()
+        local love2d = require("love2d")
+
+        if love2d.job and love2d.job.id then
+          vim.cmd.LoveStop()
+          vim.defer_fn(vim.cmd.LoveRun, 500)
+        else
+          vim.cmd.LoveRun()
+        end
+      end,
+      ft = "lua",
+      desc = "Run LÖVE"
+    },
     { "<F10>", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
   },
 }
