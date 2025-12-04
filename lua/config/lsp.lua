4f27f9027afbd0443846ffd538f9d1763c5c5f43
Spike Grobstein <me@spike.cx>
2025-12-04 09:43:45 -0800

    updated lsp config to be compatible with latest nvim

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -43,70 +43,17 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- document existing key chains
---require("which-key").register {
-    --{ "<leader>c", group = "[C]ode" },
-    --{ "<leader>d", group = "[D]ocument" },
-    --{ "<leader>g", group = "[G]it" },
-    --{ "<leader>h", group = "More git" },
-    --{ "<leader>r", group = "[R]ename" },
-    --{ "<leader>w", group = "[W]orkspace" },
-  --}
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
-local servers = {
-  clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  --rust_analyzer = {
-    --keys = {
-      --{ 'K', '<cmd>RustHoverActions<cr>', desc = 'Hover Actions (Rust)' },
-      --{ '<leader>cR', '<cmd>RustCodeAction<cr>', desc = 'Code Action (Rust)' },
-      --{ '<leader>dr', '<cmd>RustDebuggables<cr>', desc = 'Run Debuggables (Rust)' },
-      --{ '<F4>', '<cmd>RustOpenExternalDocs<cr>', { desc = 'Open documentation', remap = true } },
-      --{ '<F5>', '<cmd>RustReloadWorkspace<CR>', { desc = 'Reload the rust workspace', remap = true } },
-      --{ '<F6>', '<cmd>RustCodeAction<CR>', { desc = 'Show code action menu.', remap = true } },
-    --},
-    --settings = {
-      --['rust-analyzer'] = {
-        --cargo = {
-          --allFeatures = true,
-          --loadOutDirsFromCheck = true,
-          --runBuildScripts = true,
-        --},
-        ---- Add clippy lints for Rust.
-        --checkOnSave = {
-          --allFeatures = true,
-          --command = 'clippy',
-          --extraArgs = { '--no-deps' },
-        --},
-        --procMacro = {
-          --enable = true,
-          --ignored = {
-            --['async-trait'] = { 'async_trait' },
-            --['napi-derive'] = { 'napi' },
-            --['async-recursion'] = { 'async_recursion' },
-          --},
-        --},
-      --},
-    --},
-  --},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
+-- set up language servers
+vim.lsp.config('lua_ls', {
+  settings = {
+    lua_ls = {
+      Lua = {
+        workspace = { checkThirdParty = false },
+        telemetry = { enable = false },
+      },
     },
-  },
-}
+  }
+})
 
 -- Setup neovim lua configuration
 require('neodev').setup()
@@ -114,25 +61,9 @@ require('neodev').setup()
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
 require('mason').setup()
-
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup({
-  ensure_installed = vim.tbl_keys(servers),
-})
+require('mason-lspconfig').setup()
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
-}
