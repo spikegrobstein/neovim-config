63285706e37f3e8714c9448488017b08611ef06a
Spike Grobstein <me@spike.cx>
2026-07-01 14:17:40 -0700

    switch to a more supported bats neovim plugin

--- lua/plugins/bats.lua
+++ lua/plugins/bats.lua
@@ -1,3 +1,7 @@
 return {
-  "markcornick/vim-bats"
+  -- Syntax highlighting, filetype detection, and test-running commands for
+  -- bats (the Bash Automated Testing System). Replaces the unmaintained
+  -- markcornick/vim-bats.
+  'aliou/bats.vim',
+  ft = 'bats',
 }
